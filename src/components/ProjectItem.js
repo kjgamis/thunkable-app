852a421481eb4c9698fa92d03b85b3453af93b75
kjgamis <kjgamis@live.ca>
2023-03-04 21:00:21 -0500

    feat: add delete modal and icon to edit and delete buttons

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { Card, Button } from 'antd'
+import { Card, Button, Modal } from 'antd'
+import Icon from '@ant-design/icons'
+const { confirm } = Modal
 import { updateProject, deleteProject } from '../redux/project'
 import ProjectForm from './ProjectForm'
+import { ReactComponent as DeleteIconSvg } from '../assets/DeleteIcon.svg'
+import { ReactComponent as EditIconSvg } from '../assets/EditIcon.svg'
+import { ReactComponent as QuestionIconSvg } from '../assets/Question.svg'
 
 const ProjectItem = ({ id, name, dateCreated }) => {
   const dispatch = useDispatch()
@@ -12,9 +17,21 @@ const ProjectItem = ({ id, name, dateCreated }) => {
     dispatch(updateProject({ name, id }))
     setEditProject(false)
   }
-  
+
   const handleDeleteProject = () => {
     dispatch(deleteProject(name))
+  }
+
+  const showDeleteConfirm = () => {
+    confirm({
+      icon: <Icon component={QuestionIconSvg} />,
+      title: 'Are you sure you want to delete this project?',
+      content: 'This action cannot be undone',
+      width: 500,
+      okText: 'Yes',
+      cancelText: 'No',
+      onOk() { handleDeleteProject() }
+    })
   }
 
   return (
@@ -27,13 +44,20 @@ const ProjectItem = ({ id, name, dateCreated }) => {
         <div>
           {name}
           <Button
-            onClick={() => setEditProject(editProject => !editProject)}>
-            Edit
-          </Button>
+            onClick={() => setEditProject(editProject => !editProject)}
+            ghost
+            size='small'
+            icon={<EditIconSvg />}
+          />
         </div>
       )}
       {dateCreated}
-      <Button onClick={handleDeleteProject}>Delete</Button>
+      <Button
+        onClick={() => showDeleteConfirm()}
+        ghost
+        size='small'
+        icon={<DeleteIconSvg />}
+      />
     </Card>
   )
 }
